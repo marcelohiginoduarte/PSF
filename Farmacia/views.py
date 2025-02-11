0e14e8eb74ea68ae42bb956b0805289b8b94f514
marcelohiginoduarte <marcelihigino@gmail.com>
2025-02-11 11:32:40 -0300

    Incluindo o bootstrap

--- Farmacia/views.py
+++ Farmacia/views.py
@@ -3,6 +3,14 @@ from rest_framework import viewsets
 from .models import EstoqueFarmacia
 from .serializer import EstoqueFarmaciaSerializer
 
+
+def home(request):
+    return render(request, 'index.html')
+
+def estoque_farmacia_lista(request):
+    estoque_itens = EstoqueFarmacia.objects.all()
+    return render(request, 'Farmacia_todos_itens.html', {'estoque_itens':estoque_itens})
+
 class EstoqueFarmaciaViewSet(viewsets.ModelViewSet):
     queryset = EstoqueFarmacia.objects.all()
     serializer_class = EstoqueFarmaciaSerializer
--- psf/urls.py
+++ psf/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from Farmacia.views import EstoqueFarmaciaViewSet
+from Farmacia.views import EstoqueFarmaciaViewSet, home, estoque_farmacia_lista
 
 
 router = DefaultRouter()
@@ -10,6 +10,8 @@ router.register(r'farmacia', EstoqueFarmaciaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', home, name='home'),
+    path('Farmacia/estoque', estoque_farmacia_lista, name='estoque farmacia'),
     path('', include(router.urls)),
     path('api/', include(router.urls)),
 ]
